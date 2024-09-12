a15d44a7dc72c627d5f540fc85902ae93a98f036
Calvin Bonner <me@calvinbonner.com>
2024-09-12 11:22:04 -0400

    initial commit

---
+++ src/lib/generate-id.ts
@@ -0,0 +1,15 @@
+export class UniqueId {
+  private prefix: string = '';
+  private suffix: string = '';
+
+  constructor(
+    prefix?: string,
+    suffix?: string
+  ) {
+    this.prefix = prefix ?? this.prefix;
+    this.suffix = suffix ?? this.suffix;
+  }
+
+	public id = `${this.prefix ? `${this.prefix}-` : ''}${(new Date().getTime() * (Math.random() * 36)).toString(36).substring(2, 5)}-${(new Date().getTime() * (Math.random() * 64)).toString(36).substring(2, 5)}${this.suffix ? `-${this.suffix}` : ''}`;
+}
+
