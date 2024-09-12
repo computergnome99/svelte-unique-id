53a6d2a20bef71806d80168fa66c6e342cc05392
Calvin Bonner <me@calvinbonner.com>
2024-09-12 11:24:23 -0400

    pushing for inital publish

--- src/lib/unique-id.ts
+++ src/lib/unique-id.ts
@@ -1,15 +1,11 @@
 export class UniqueId {
-  private prefix: string = '';
-  private suffix: string = '';
+	private prefix: string = '';
+	private suffix: string = '';
 
-  constructor(
-    prefix?: string,
-    suffix?: string
-  ) {
-    this.prefix = prefix ?? this.prefix;
-    this.suffix = suffix ?? this.suffix;
-  }
+	constructor(prefix?: string, suffix?: string) {
+		this.prefix = prefix ?? this.prefix;
+		this.suffix = suffix ?? this.suffix;
+	}
 
 	public id = `${this.prefix ? `${this.prefix}-` : ''}${(new Date().getTime() * (Math.random() * 36)).toString(36).substring(2, 5)}-${(new Date().getTime() * (Math.random() * 64)).toString(36).substring(2, 5)}${this.suffix ? `-${this.suffix}` : ''}`;
 }
-
